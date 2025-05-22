8f836be9302531e2cb64b42af12c6d8ea654b2bd
Tanvir Ahmed <tanvirahmedmd994@gmail.com>
2025-05-22 03:32:21 -0600

    basic of dom

--- script.js
+++ script.js
@@ -22,10 +22,59 @@
 
 
 //    <!-- finding html elements by html object collection -->
-const x = document.forms['frm1'];
+// const x = document.forms["frm1"];
+// let text = "";
+// for (let i = 0; i < x.length ;i++) {
+//   text = text + x.elements[i].value + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text;
 
-let text = " ";
-for (let i = 0; i <= x.length; i++){
-    text = text + x.element[i].value;
-}
-document.getElementById('demo').innerHTML = text;
+
+//   <!-- DOM html -->
+// <!-- changing the attribute value -->
+// const element = document.getElementById('myImage');
+
+// element.src = 'tanvir.jpg'
+// console.log(element);
+
+
+// dynamic html content
+// const element = document.getElementById('demo');
+// console.log(element);
+
+// element.innerHTML = Date();
+// element.innerHTML = 'Today\'s date is:' + Date();
+// template literal
+// element.innerHTML = `Today's date is: ${Date()} `;
+
+
+//  <!-- dom html form -->
+// function validInputForm() {
+//     const form = document.forms['myForm'];
+//     const value = form['fname'].value;
+    
+//     if (value === "") {
+//         alert("you must fill up name");
+
+//         return false;
+//     }
+// }
+
+// Please input a number between 1 and 15
+// function numberValidCheck() {
+//     let x = document.getElementById('numb').value;
+    
+//     let text;
+//     if (isNaN(x) || x < 1 || x > 15) {
+//         text = "Invalid input"
+//     } else {
+//         text = "valid input";
+//     }
+//     document.getElementById('demo').innerHTML = text;
+// } 
+
+// dom css 
+// const styling = document.getElementById('pera');
+
+// styling.style.color = "red";
+// styling.style.fontSize = "30px"
